883c1de9c4b94e6d66b113511c6db0442f2135a4
MetalDent <priyapandya274@gmail.com>
2021-11-09 20:41:05 +0530

    -Edit narrow-bridge

--- src/tests/threads/narrow-bridge.c
+++ src/tests/threads/narrow-bridge.c
@@ -8,22 +8,21 @@
 #include "threads/synch.h"
 #include "threads/thread.h"
 
-#define LEFT 0
-#define RIGHT 1
-int currentDirection = LEFT;
-struct lock lock;
-struct semaphore space, mutex, direction[2];
-int active = 0, waiting = 0, counter = 0;
+#define LEFT 		0
+#define RIGHT 	1
 
+int currentDirection = LEFT;
+int active = 0;
+int waiting = 0; 
+int counter = 0;
+
+struct lock lock_res;
+struct semaphore space;
+struct semaphore mutex; 
+struct semaphore direction[2];
 
 void narrow_bridge(unsigned int num_vehicles_left, unsigned int num_vehicles_right,
         unsigned int num_emergency_left, unsigned int num_emergency_right);
-
-int Reverse(int myDir)
-{
-    if(myDir == LEFT)return RIGHT;
-    return LEFT;
-}
 
 void test_narrow_bridge(void)
 {
@@ -45,83 +44,88 @@ void test_narrow_bridge(void)
     pass();
 }
 
-void BridgeInit(void)
+void BridgeInit(void)							// initiating semaphores
 {
-    sema_init(&space,3);
-    sema_init(&mutex,1);
-    sema_init(&direction[LEFT],0);
-    sema_init(&direction[RIGHT],0);
+  sema_init(&space,3);
+  sema_init(&mutex,1);
+  sema_init(&direction[LEFT],0);
+  sema_init(&direction[RIGHT],0);
 }
 
-// static int isSafe(int Direction)
-// {
-//   if (vehicles == 0)                // check if no vehicle on bridge
-//     return  TRUE;                   // then it's safe to cross            
-//  else if ((vehicles < MAX_VEHICLE) && (cur_direc == Direction))
-//    return  TRUE;                    // if < 3 in same direction 
-//   else
-//     return  FALSE;                  // do not proceed
-// }
-
-void ArriveBridge(int myDir)
+int Reverse(int direc)							// reverses the direction of the vehicle
 {
-    int vehicle_num = thread_tid();
-    int priority = thread_get_priority();
-    printf("Vehicle %d, direction: %d, priority %d Arrived!\n", vehicle_num, myDir, priority);
-    sema_down(&mutex);
-    if (currentDirection != myDir && active > 0)
-    {
-        printf("Waiting\n");
-        waiting++; // need to wait
-        sema_up(&mutex);
-        sema_down(&direction[myDir]); //when wake up, ready to go!
-    } 
-    else 
-    {
-        currentDirection = myDir;
-        active++;
-        sema_up(&mutex);
-    }
-    sema_down(&space); // make sure there is space on bridge
+    if(direc == LEFT)
+			return RIGHT;
+    return LEFT;
 }
 
-void CrossBridge(int myDir)
+void ArriveBridge(int direc, int prio)
 {
-    int priority = thread_get_priority();
-    int vehicle_num = thread_tid();
-    counter++;
-  printf("Vehicle %d, direction: %d, priority %d, Entered!\n", vehicle_num, myDir, priority);
+  int vehicle_num = thread_tid();
+  
+	printf("Vehicle %d, direction: %d, priority %d Arrived!\n", vehicle_num, direc, prio);
+  
+	sema_down(&mutex);
+  
+	if (currentDirection != direc && active > 0)
+  {
+		printf("Waiting\n");
+    waiting++; 											// need to wait
+    sema_up(&mutex);
+    sema_down(&direction[direc]); 	// when wake up, ready to go!
+  } 
+  else 
+  {
+    currentDirection = direc;
+    active++;
+    sema_up(&mutex);
+  }
+	
+	sema_down(&space); 								// make sure there is space on bridge
+}
+
+void CrossBridge(int direc, int prio)
+{
+  int vehicle_num = thread_tid();
+  
+	counter++;
+  
+	printf("Vehicle %d, direction: %d, priority %d, Entered!\n", vehicle_num, direc, prio);
   printf("%d\n", counter);
-  thread_yield();
+  
+	thread_yield();
 }
 
-void ExitBridge(int myDir)
+void ExitBridge(int direc, int prio)
 {
-    counter--;
-    sema_up(&space);
-    sema_down(&mutex);
-    if (active-- == 0) 
+  counter--;
+  sema_up(&space);
+  sema_down(&mutex);
+  if (active-- == 0) 
+  {
+		while (waiting > 0) 
     {
-        while (waiting > 0) 
-        {
-            waiting--;
-            active++;
-            currentDirection = Reverse(myDir);
-            sema_up(&direction[Reverse(myDir)]);
-        }
+			waiting--;
+      active++;
+      currentDirection = Reverse(direc);
+      sema_up(&direction[Reverse(direc)]);
     }
-    sema_up(&mutex); 
-    int priority = thread_get_priority();
-    int vehicle_num = thread_tid();
-    printf("Vehicle %d, direction: %d, priority: %d Exited!\n", vehicle_num, myDir, priority);
+  }
+  sema_up(&mutex); 
+
+  int vehicle_num = thread_tid();
+  printf("Vehicle %d, direction: %d, priority: %d Exited!\n", vehicle_num, direc, prio);
 }
 
-void OneVehicle(int myDir)
+void OneVehicle(int direc)
 {
-  ArriveBridge(myDir);              // arrive at the bridge     
-  CrossBridge(myDir);               // crossing the bridge 
-  ExitBridge(myDir);                // exit the bridge          
-  thread_exit();
+  int prio = thread_get_priority();				// get priority of the vehicle
+	
+	ArriveBridge(direc, prio);              // arrive at the bridge     
+  CrossBridge(direc, prio);               // crossing the bridge 
+  ExitBridge(direc, prio);                // exit the bridge          
+  
+	thread_exit();
 }
 
 void narrow_bridge(UNUSED unsigned int num_vehicles_left, UNUSED unsigned int num_vehicles_right,
@@ -152,8 +156,9 @@ void narrow_bridge(UNUSED unsigned int num_vehicles_left, UNUSED unsigned int nu
     }      
   }
   
-    for (int i = 0; i < num_vehicles_left; i++) 
-      thread_create("left", 0, OneVehicle, (void *)LEFT);
-    for (int i = 0; i < num_vehicles_right; i++) 
-      thread_create("right", 0, OneVehicle, (void *)RIGHT);
+	for (int i = 0; i < num_vehicles_left; i++) 															// create threads for non-priority left going vehicles
+    thread_create("left", 0, OneVehicle, (void *)LEFT);
+  
+	for (int i = 0; i < num_vehicles_right; i++) 															// create threads for non-priority right going vehicles
+    thread_create("right", 0, OneVehicle, (void *)RIGHT);
 }
